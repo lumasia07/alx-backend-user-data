5b9923a8ff914e2f4050483b2d20a6cab3e2af63
Lumasia <lumasiastancelaus@gmail.com>
2024-08-29 16:08:02 +0300

    filter logger

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Returns log msg obfuscated"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    pattern = '|'.join([f"{field}=[^{separator}]*" for field in fields])
+    return re.sub(pattern, lambda m: f"{m.group().split('=')[0]}={redaction}",
+                  message)
