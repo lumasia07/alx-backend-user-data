b6a0335265a17a027d8211026a32def32e9387ce
Lumasia <lumasiastancelaus@gmail.com>
2024-08-29 22:45:56 +0300

    encrypt password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Encrypt a paswword"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Return hashed password"""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
