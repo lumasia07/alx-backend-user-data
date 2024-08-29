4936e0ce2dbda2e05e5ab840d9f3e258b21863bc
Lumasia <lumasiastancelaus@gmail.com>
2024-08-29 16:09:15 +0300

    filter logger fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,7 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
+    """Filters datum using regex"""
     pattern = '|'.join([f"{field}=[^{separator}]*" for field in fields])
     return re.sub(pattern, lambda m: f"{m.group().split('=')[0]}={redaction}",
                   message)
