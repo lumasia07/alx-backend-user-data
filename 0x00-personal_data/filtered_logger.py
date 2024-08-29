cb82f7a4ef1ac2fb9bad4a48990ffe213211fa57
Lumasia <lumasiastancelaus@gmail.com>
2024-08-29 21:22:08 +0300

    Number of fields

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """Returns log msg obfuscated"""
 import re
-from typing import List
+from typing import List, Tuple
 import logging
+
+
+PII_FIELDS: Tuple[str, ...] = ("name", "email", "phone", "ssn", "password")
 
 
 class RedactingFormatter(logging.Formatter):
@@ -30,3 +33,18 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     pattern = '|'.join([f"{field}=[^{separator}]*" for field in fields])
     return re.sub(pattern, lambda m: f"{m.group().split('=')[0]}={redaction}",
                   message)
+
+
+def get_logger() -> logging.Logger:
+    """Returns a logger object"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(list(PII_FIELDS))
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    return logger
