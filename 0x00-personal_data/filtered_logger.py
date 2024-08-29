8d5f692210032dc1cf646f5886498fce6afc6e2f
Lumasia <lumasiastancelaus@gmail.com>
2024-08-29 17:18:22 +0300

    Logger info

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,24 @@
 """Returns log msg obfuscated"""
 import re
 from typing import List
+import logging
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialize the formatter with the list of fields to redact"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format the log record and apply redaction"""
+        original_message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, original_message, self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
