c6b527f274e44898d5077ec4fff40ed8d2c21886
Lumasia <lumasiastancelaus@gmail.com>
2024-09-02 14:28:56 +0300

    Blueprint for Auth file

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+from flask import request
+from typing import List, TypeVar
+"""Module for authorization"""
+
+
+class Auth:
+    """Defines a class Auth"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Returns False"""
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """Returns None"""
+        return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns None"""
+        return None
+
+    
