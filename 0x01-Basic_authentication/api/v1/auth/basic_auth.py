678b4d4fac85372522331d8f01aef1081d80ace6
Lumasia <lumasiastancelaus@gmail.com>
2024-09-03 12:32:57 +0300

    Rectified logic for implementing auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Module Basic Auth that inherits from Auth"""
-from auth import Auth
+from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
     """inherits from Base Class Auth"""
+    pass
