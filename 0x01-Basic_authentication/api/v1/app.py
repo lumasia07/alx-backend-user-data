ecd924457b1bae853c7299eb0435a866459ab12b
Lumasia <lumasiastancelaus@gmail.com>
2024-09-03 20:01:31 +0300

    Print user details

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,7 +20,7 @@ if auth_type:
     if auth_type == 'basic_auth':
         from api.v1.auth.basic_auth import BasicAuth
         auth = BasicAuth()
-    else:
+    elif auth_type == 'auth':
         from api.v1.auth.auth import Auth
         auth = Auth()
 
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,8 +2,11 @@
 """Module Basic Auth that inherits from Auth"""
 import base64
 from api.v1.auth.auth import Auth
+from typing import TypeVar
+from models.user import User
 
 
+User_1 = TypeVar('User')
 class BasicAuth(Auth):
     """inherits from Base Class Auth"""
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
@@ -47,4 +50,21 @@ class BasicAuth(Auth):
         email, pwd = decoded_base64_authorization_header.split(':', 1)
         return email, pwd
 
-     
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> User_1:
+        """Returns user based on email and password"""
+        if not isinstance(user_email, str) or user_email is None:
+            return None
+        
+        if not isinstance(user_pwd, str) or user_pwd is None:
+            return None
+        
+        user = User.search({'email': user_email})
+
+        if not user:
+            return None
+        
+        
+        if user.is_valid_password(user_pwd):
+            return None
+        
+        return user
