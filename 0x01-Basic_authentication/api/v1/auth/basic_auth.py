0b14f42e3d364bd26dddcaf6d1ae4aaf036a9ed1
Lumasia <lumasiastancelaus@gmail.com>
2024-09-03 20:27:21 +0300

    Basic Auth implemented

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -72,3 +72,27 @@ class BasicAuth(Auth):
         return user
 
 
+    def current_user(self, request=None) -> User_1:
+        """Retrieves the User instance for a request."""
+        if request is None:
+            return None
+        
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+
+        base64_auth_header = self.extract_base64_authorization_header(auth_header)
+        if base64_auth_header is None:
+            return None
+
+        decoded_auth = self.decode_base64_authorization_header(base64_auth_header)
+        if decoded_auth is None:
+            return None
+
+        user_email, user_pwd = self.extract_user_credentials(decoded_auth)
+        if user_email is None or user_pwd is None:
+            return None
+
+        user = self.user_object_from_credentials(user_email, user_pwd)
+        return user
+
