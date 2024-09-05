4e4f04df989452bd674a1b0324152332d52ac587
Lumasia <lumasiastancelaus@gmail.com>
2024-09-05 10:27:00 +0300

    Added a route users/me and basic logic for session auth

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -6,6 +6,19 @@ from flask import abort, jsonify, request
 from models.user import User
 
 
+@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
+def get_current_user() -> str:
+    """ GET /api/v1/users/me
+    Return:
+      - Authenticated User object JSON represented
+      - 404 if no authenticated user
+    """
+    if request.current_user is None:
+        abort(404)
+    return jsonify(request.current_user.to_json())
+
+
+
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def view_all_users() -> str:
     """ GET /api/v1/users
@@ -25,9 +38,16 @@ def view_one_user(user_id: str = None) -> str:
       - User object JSON represented
       - 404 if the User ID doesn't exist
     """
+    if user_id is "me":
+        if request.current_user is None:
+            abort(404)
+        return jsonify(request.current_user.to_json())
+
     if user_id is None:
         abort(404)
+
     user = User.get(user_id)
+
     if user is None:
         abort(404)
     return jsonify(user.to_json())
