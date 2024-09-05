a1d0ebe84123ddbb854eb311af8d578902a4b750
Lumasia <lumasiastancelaus@gmail.com>
2024-09-05 10:47:49 +0300

    Added logic to handle env for session Auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -23,6 +23,9 @@ if auth_type:
     elif auth_type == 'auth':
         from api.v1.auth.auth import Auth
         auth = Auth()
+    elif auth_type == 'session_auth':
+        from api.v1.auth.session_auth import SessionAuth
+        auth = SessionAuth()
 
 EXCLUDED_PATHS = ['/api/v1/status/', '/api/v1/unauthorized/',
                   '/api/v1/forbidden/']
