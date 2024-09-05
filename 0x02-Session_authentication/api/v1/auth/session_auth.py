3f76554391e8ff16667baf6335b64e2249d60846
Lumasia <lumasiastancelaus@gmail.com>
2024-09-05 11:11:07 +0300

    session auth update

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 """Module for Session auth"""
+import uuid
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
     """Defines session auth"""
-    pass
+    def __init__(self):
+        """Initialize user by session id"""
+        self.user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create user session"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
