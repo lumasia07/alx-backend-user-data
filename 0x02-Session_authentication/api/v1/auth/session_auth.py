6b505338c1a52a6c78bae4c6c17d7678ed78bc8d
Lumasia <lumasiastancelaus@gmail.com>
2024-09-07 12:20:55 +0300

    sessionAuth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -28,3 +28,13 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """Return user based on session cookie"""
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+
+        if user_id:
+            return User.get(user_id)
+        return None
+
