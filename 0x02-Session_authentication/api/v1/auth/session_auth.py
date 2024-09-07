9a37ac172bf89b1141e180c2231ad5577b6ed30e
Lumasia <lumasiastancelaus@gmail.com>
2024-09-07 12:46:11 +0300

    sessipn auth route

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """Module for Session auth"""
+import os
 import uuid
+from flask import abort, jsonify, request
 from api.v1.auth.auth import Auth
 from models.user import User
+from api.v1.views import app_views
 
 
 class SessionAuth(Auth):
@@ -38,3 +41,40 @@ class SessionAuth(Auth):
             return User.get(user_id)
         return None
 
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """Handles the login route for session authentication."""
+    
+    email = request.form.get('email')
+    password = request.form.get('password')
+    
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+
+    users = User.search({'email': email})
+
+    if not users or len(users) == 0:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    user = users[0]
+    
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 40
+    from api.v1.app import auth
+    
+    session_id = auth.create_session(user.id)
+
+    if not session_id:
+        return abort(500, description="Session creation failed")
+
+    session_name = os.getenv('SESSION_NAME')
+
+    user_data = user.to_json()
+    response = jsonify(user_data)
+
+    response.set_cookie(session_name, session_id)
+
+    return response
