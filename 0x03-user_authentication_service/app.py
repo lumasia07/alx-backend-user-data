e9df02e38dba8cdd217a439bb0c6f9e6724290f4
Lumasia <lumasiastancelaus@gmail.com>
2024-09-12 18:09:38 +0300

    added post method for sessions

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Flask main entry point"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, make_response, request, abort
 from auth import Auth
 
 
@@ -29,6 +29,23 @@ def register_user() -> str:
 
     except ValueError:
         return jsonify({"message": "email already registered"})
+
+
+@app.route('/sessions', methods=['POST'])
+def login() -> str:
+    """POST /sessions route for logging in a user."""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+
+    response = make_response(jsonify({"email": email, "message": "logged in"}))
+    response.set_cookie("session_id", session_id)
+
+    return response
 
 
 if __name__ == "__main__":
