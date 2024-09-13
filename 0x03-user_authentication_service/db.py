72fe25161e29b88f75ce92448492e0e210d5f9bd
Lumasia <lumasiastancelaus@gmail.com>
2024-09-13 13:47:22 +0300

    Type annotation fix to Any

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """DB module
 """
+from typing import Any
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -47,7 +48,7 @@ class DB:
         self._session.commit()
         return new_user
 
-    def find_user_by(self, **kwargs: dict) -> User:
+    def find_user_by(self, **kwargs: Any) -> User:
         """Finds a user by arbitrary keyword arguments.
 
         Args:
