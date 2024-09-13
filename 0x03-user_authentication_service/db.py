956101cb1db2579f3a689cabedbf86003f0ed007
Lumasia <lumasiastancelaus@gmail.com>
2024-09-13 13:33:09 +0300

    Find User by specified kwargs

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 
 from user import Base, User
 
@@ -16,7 +17,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -45,25 +46,25 @@ class DB:
         self._session.commit()
         return new_user
 
-    # def find_user_by(self, **kwargs: dict) -> User:
-    #     """Finds a user by arbitrary keyword arguments.
+    def find_user_by(self, **kwargs: dict) -> User:
+        """Finds a user by arbitrary keyword arguments.
 
-    #     Args:
-    #         **kwargs (dict): Keyword arguments to filter the query.
+        Args:
+            **kwargs (dict): Keyword arguments to filter the query.
 
-    #     Returns:
-    #         User: The User object matching the query.
+        Returns:
+            User: The User object matching the query.
 
-    #     Raises:
-    #         NoResultFound: If no user matches the query.
-    #         InvalidRequestError: If the query arguments are invalid.
-    #     """
-    #     try:
-    #         return self._session.query(User).filter_by(**kwargs).one()
-    #     except NoResultFound:
-    #         raise NoResultFound("No user found with the provided arguments")
-    #     except InvalidRequestError:
-    #         raise InvalidRequestError("Invalid query arguments")
+        Raises:
+            NoResultFound: If no user matches the query.
+            InvalidRequestError: If the query arguments are invalid.
+        """
+        try:
+            return self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound("No user found with the provided arguments")
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid query arguments")
 
     # def update_user(self, user_id: int, **kwargs: dict) -> None:
     #     """Updates user attributes using the user ID.
